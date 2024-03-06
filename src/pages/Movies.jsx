e50a5e24f5267909c108be1a588817cdbca9510c
rajdhanwar-suraj <sharmasuraj747947@gmail.com>
2024-03-06 18:59:24 +0530

    final commit after fix typo 1

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,168 @@
+// import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+// import Navbar from "../components/Navbar";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { firebaseAuth } from "../utils/firebase-config";
+// import { useNavigate } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
+// import { fetchMovies, getGenres } from "../store";
+// import SelectGenre from "../components/SelectGenre";
+// import Slider from "../components/Slider";
+// import NotAvailable from "../components/NotAvailable";
+
+// function MoviePage() {
+//   const [isScrolled, setIsScrolled] = useState(false);
+//   const movies = useSelector((state) => state.netflix.movies);
+//   const genres = useSelector((state) => state.netflix.genres);
+//   const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(getGenres());
+//   }, []);
+
+//   useEffect(() => {
+//     if (genresLoaded) {
+//       dispatch(fetchMovies({ genres, type: "movie" }));
+//     }
+//   }, [genresLoaded]);
+
+//   const [user, setUser] = useState(undefined);
+
+//   onAuthStateChanged(firebaseAuth, (currentUser) => {
+//     if (currentUser) setUser(currentUser.uid);
+//     else navigate("/login");
+//   });
+
+//   window.onscroll = () => {
+//     setIsScrolled(window.pageYOffset === 0 ? false : true);
+//     return () => (window.onscroll = null);
+//   };
+
+//   return (
+//     <Container>
+//       <div className="navbar">
+//         <Navbar isScrolled={isScrolled} />
+//       </div>
+//       <div className="data">
+//         <SelectGenre genres={genres} type="movie" />
+//         {movies.length ? <Slider movies={movies} /> : <NotAvailable />}
+//       </div>
+//     </Container>
+//   );
+// }
+
+// const Container = styled.div`
+//   .data {
+//     margin-top: 8rem;
+//     .not-available {
+//       text-align: center;
+//       color: white;
+//       margin-top: 4rem;
+//     }
+//   }
+// `;
+// export default MoviePage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
-import CardSlider from "../components/CardSlider";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 import { useNavigate } from "react-router-dom";
@@ -22,25 +183,36 @@ function MoviePage() {
 
   useEffect(() => {
     dispatch(getGenres());
-  }, []);
+  }, [dispatch]); // Add dispatch as a dependency
 
   useEffect(() => {
     if (genresLoaded) {
       dispatch(fetchMovies({ genres, type: "movie" }));
     }
-  }, [genresLoaded]);
+  }, [genresLoaded, genres, dispatch]); // Add genresLoaded, genres, and dispatch as dependencies
 
   const [user, setUser] = useState(undefined);
 
-  onAuthStateChanged(firebaseAuth, (currentUser) => {
-    if (currentUser) setUser(currentUser.uid);
-    else navigate("/login");
-  });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(firebaseAuth, (currentUser) => {
+      if (currentUser) setUser(currentUser.uid);
+      else navigate("/login");
+    });
 
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null);
-  };
+    return () => unsubscribe(); // Cleanup the subscription
+  }, [navigate]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.pageYOffset === 0 ? false : true);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll); // Cleanup the event listener
+    };
+  }, []);
 
   return (
     <Container>
@@ -65,4 +237,5 @@ const Container = styled.div`
     }
   }
 `;
+
 export default MoviePage;
